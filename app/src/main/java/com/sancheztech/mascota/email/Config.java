32c83e81cce1fd9828c1d323bd2444a2ca836a67
sanchezgtz <ricardo.sanchezgtz@gmail.com>
2016-05-18 17:24:51 -0500

    se sube proyecto de semana 4, javamail. fragment,

---
+++ app/src/main/java/com/sancheztech/mascota/email/Config.java
@@ -0,0 +1,10 @@
+package com.sancheztech.mascota.email;
+
+/**
+ * Created by rsanchezgu on 16/05/2016.
+ */
+public class Config {
+    //-- colocar usuario y contraseña
+    public static final String EMAIL = "algunacuenta@gmail.com";
+    public static final String PASSWORD = "contraseña";
+}
